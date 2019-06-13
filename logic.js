b8beadd6d80bddd22691eca4ec9b74449edc6d0f
The-Ryan-Mobley <49420591+The-Ryan-Mobley@users.noreply.github.com>
2019-06-12 20:09:40 -0500

    fixed error causing all cells to deactivate

--- logic.js
+++ logic.js
@@ -120,7 +120,7 @@ function newgeneration(){
     for (let i = 0; i < 2547; i++) {
         
         cellarray[i].neighbors = 0;
-        if(cellarray[i].life >= 5){
+        if(cellarray[i].life >= 2){
             cellarray[i].life =0;
         }
     }
